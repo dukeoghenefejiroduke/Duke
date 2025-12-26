85dd402d0d2d8438a1d42ba7a807534d99d6a8a3
dukeoghenefejiroduke <dukeoghenefejiroduke@gmail.com>
2025-12-26 23:20:13 +0100

    Full Android platform regenerate + custom JARVIS icon + fixed background-runner repo

--- src/services/wakeword.js
+++ src/services/wakeword.js
@@ -1,26 +1,36 @@
 // src/services/wakeword.js
-import PorcupineWorker from "@picovoice/porcupine-web/dist/porcupine-web-worker.js";
-
 let porcupineWorker = null;
 
 export const initWakeWord = async (onWakeWordDetected) => {
   if (porcupineWorker) return;
 
-  porcupineWorker = new PorcupineWorker({
+  // Dynamically create a Web Worker using the Porcupine Web worker URL
+  const workerUrl = new URL(
+    "@picovoice/porcupine-web/dist/porcupine-web-worker.min.js",
+    import.meta.url
+  );
+
+  porcupineWorker = new Worker(workerUrl, { type: "module" });
+
+  porcupineWorker.postMessage({
+    command: "init",
     keywords: ["jarvis"],
     sensitivities: [0.7],
-    onKeywordDetected: (keywordIndex) => {
-      onWakeWordDetected();
-    },
   });
 
+  porcupineWorker.onmessage = (e) => {
+    if (e.data && e.data.keywordIndex !== undefined) {
+      onWakeWordDetected();
+    }
+  };
+
   // Start microphone capture
-  await porcupineWorker.start();
+  porcupineWorker.postMessage({ command: "start" });
 };
 
 export const stopWakeWord = async () => {
   if (porcupineWorker) {
-    await porcupineWorker.stop();
+    porcupineWorker.postMessage({ command: "stop" });
     porcupineWorker.terminate();
     porcupineWorker = null;
   }
